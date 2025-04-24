f86cd843154cec7eceb07ed0a3a438e9b169c1cf
Moyunlimited <moy.estime@gmail.com>
2025-04-24 15:37:13 +0000

    fix

--- src/app.py
+++ src/app.py
@@ -46,10 +46,10 @@ def add_new_todo():
 def get_todos():
     return jsonify(todos)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True) 
-    
 @app.route('DELETE/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete:", position)
     return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True) 
